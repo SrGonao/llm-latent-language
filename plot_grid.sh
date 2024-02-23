b6c07964f9c714791bafa91e98a66c554a80ad8d
SrGonao <goncalo.paulo@uniroma1.it>
2024-02-23 14:03:29 +0000

    Weird

--- plot_grid.sh
+++ plot_grid.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-for size in '7b' 
+for size in '7b'
 do
    for input in 'zh' 'fr' 'de' 'ru' 'en' 
    do 
        for output in 'zh' 'fr' 'de' 'ru' 'en'
        do 
            echo "size: $size, input: $input, output: $output"
-           papermill Translation.ipynb visuals/executed_notebooks/Translation_Final_${size}_${input}_${output}.ipynb -p model_size $size -p target_lang $output -p input_lang $input
+           papermill Translation.ipynb visuals/executed_notebooks/Translation_Chinese_${size}_${input}_${output}.ipynb -p model_size $size -p target_lang $output -p input_lang $input
        done 
    done 
 done 
 
-for size in '7b'
-do
-    for output in 'zh' 'fr' 'de' 'ru' 'en'
-    do 
-        echo "size: $size, output: $output"
-        papermill Cloze.ipynb visuals/executed_notebooks/Cloze_Final_${size}_${output}.ipynb -p model_size $size -p target_lang $output -p input_lang $output
-    done
-done
+#for size in '7b' '13b'
+#do
+#    for output in 'zh' 'fr' 'de' 'ru' 'en'
+#    do 
+#        echo "size: $size, output: $output"
+#        papermill Cloze.ipynb visuals/executed_notebooks/Cloze_Final_${size}_${output}.ipynb -p model_size $size -p target_lang $output -p input_lang $output
+#    done
+#done
